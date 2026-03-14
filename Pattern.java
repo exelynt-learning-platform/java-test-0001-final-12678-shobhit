2e39a52319825ae8c67ac6e07fce70f21f3c76a0
Shobhit Gaidhane <155177835+shobhit0663@users.noreply.github.com>
2026-03-14 14:42:10 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -1,19 +1,33 @@
 public class Pattern {
+    private static final int PATTERN_SIZE = 5;
+
     public static void main(String[] args) {
-        int n = 5;
+        int n = PATTERN_SIZE;
+
         for (int i = 1; i <= n; i++) {
-            for (int j = i; j < n; j++) System.out.print(" ");
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
             for (int j = 1; j <= (2 * i - 1); j++) {
-                if (j == 1 || j == (2 * i - 1)) System.out.print("*");
-                else System.out.print(" ");
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
+
         for (int i = n - 1; i >= 1; i--) {
-            for (int j = n; j > i; j--) System.out.print(" ");
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
             for (int j = 1; j <= (2 * i - 1); j++) {
-                if (j == 1 || j == (2 * i - 1)) System.out.print("*");
-                else System.out.print(" ");
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
